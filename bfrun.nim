0d829bdede06aaee404088ebc2a634b14ab9e72b
tangentstorm <michal.wallace@gmail.com>
2013-05-30 19:51:59 -0400

    tests for < and >

--- bfrun.nim
+++ bfrun.nim
@@ -1,6 +1,6 @@
 # brainfuck interpreter in nimrod
 # http://en.wikipedia.org/wiki/Brainfuck
-import os, unsigned, terminal, unittests
+import os, unsigned, terminal, unittest
 
 const
   kRamSize = 64
@@ -13,62 +13,35 @@ type
 proc emit(x:char) =
   write(stdout,x)
 
-proc main(path:string) =
-  var
-    data : TData
-    ip  : int = 0 # instruction pointer
-    dp  : int = 0 # data pointer
+var
+    data   : TData
+    ip     : int = 0 # instruction pointer
+    dp     : int = 0 # data pointer
     output : string = ""
-  let
-    code = readFile(path)
+    code   : TCode 
 
-  # replace ascii control codes with the
-  # equivalent caret representation
-  proc drawOutput() =
-    for i in countup(0, len(output)):
-      if ord(output[i]) == 0:
-        setForegroundColor(fgBlack, true)
-        emit('.')
-        resetAttributes()
-      elif ord(output[i]) < 32:
-        setForegroundColor(fgRed, true)
-        emit('^'); emit( chr(ord(output[i]) + ord('@')))
-        resetAttributes()
-      else: emit(output[i])
-    echo()
+proc fwd =
+  var bal : int = 0; var done : bool
+  while ip < len(code) and not done:
+    if code[ip] == '[': inc(bal)
+    elif code[ip] == ']':
+      dec(bal)
+      done = bal == 0
+    inc(ip)
+  if not done: raise newException(E_base, "No matching ] found.")
 
-  proc draw =
-    EraseScreen()
-    setCursorPos(0,0)
-    # TODO drawData()
-    # show the code, but ignore comment chars
-    for i in countup(0, len(code)):
-      if i == ip: setStyle({styleReverse})
-      emit(code[i])
-      if i == ip: resetAttributes()
-    drawOutput()
-    echo "--"
+proc bak =
+  var bal : int = 0; var done : bool
+  while not done:
+    if code[ip] == ']': inc(bal)
+    elif code[ip] == '[':
+      dec(bal)
+      done = bal == 0
+    if ip == 0: raise newException(E_base, "No matching [ found.")
+    else: dec(ip)
 
-  proc fwd =
-    var bal : int = 0; var done : bool
-    while ip < len(code) and not done:
-      if code[ip] == '[': inc(bal)
-      elif code[ip] == ']':
-        dec(bal)
-        done = bal == 0
-      inc(ip)
-    if not done: raise newException(E_base, "No matching ] found.")
 
-  proc bak =
-    var bal : int = 0; var done : bool
-    while not done:
-      if code[ip] == ']': inc(bal)
-      elif code[ip] == '[':
-        dec(bal)
-        done = bal == 0
-      if ip == 0: raise newException(E_base, "No matching [ found.")
-      else: dec(ip)
-
+proc runTime(code:string) =
   while ip < len(code):
     if code[ip] in kOpcodes:
       case code[ip]
@@ -83,18 +56,57 @@ proc main(path:string) =
       of ']' :
         if data[dp]==0: bak()
       else   : nil
-      draw()
-      discard readChar(stdin)
+      #     draw()
+      #     discard readChar(stdin)
     else:nil
     inc(ip)
+  echo(output)
+
+# replace ascii control codes with the
+# equivalent caret representation
+# proc drawOutput() =
+#   for i in countup(0, len(output)):
+#     if ord(output[i]) == 0:
+#       setForegroundColor(fgBlack, true)
+#       emit('.')
+#       resetAttributes()
+#     elif ord(output[i]) < 32:
+#       setForegroundColor(fgRed, true)
+#       emit('^'); emit( chr(ord(output[i]) + ord('@')))
+#       resetAttributes()
+#     else: emit(output[i])
+#   echo()
+
+# proc draw =
+#   EraseScreen()
+#   setCursorPos(0,0)
+#   # TODO drawData()
+#   # show the code, but ignore comment chars
+#   for i in countup(0, len(code)):
+#     if i == ip: setStyle({styleReverse})
+#     emit(code[i])
+#     if i == ip: resetAttributes()
+#   drawOutput()
+# echo "--"
+
+proc main(path:string) =
+  runTime readFile(path)
 
 proc runTests =
   test "> increments dp":
-    nil # TODO
-
+    dp = 0
+    runTime(">")
+    check dp == 1
+  
+  test "> decrements dp":
+    dp = 0; runtime(">")
+    runtime("<")
+    check dp == 0
+    
 # command line interface:
 when isMainModule:
-  if paramcount() = 1:
+
+  if paramcount() == 1:
     if paramstr(1) == "-t": runTests()
     else: main(paramstr(1))
   else: echo("usage: bfrun [-t|PATH]")
