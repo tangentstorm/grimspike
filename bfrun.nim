7a4156cb3b065a769eea82c1ea922f5a54616ab6
tangentstorm <michal.wallace@gmail.com>
2013-05-30 17:42:09 -0400

    split ram into code and data (still broken though :/)

--- bfrun.nim
+++ bfrun.nim
@@ -6,29 +6,21 @@ const
   kRamSize = 30000
 
 type
-  TRam = array[0..kRamSize, byte]
-
-var
-  ram : TRam
-
-proc readCode(path:string) =
-  var
-    f  : TFile = open(path)
-    i  : int = 0
-  while i < kRamSize and not EndOfFile(f):
-    ram[i] = ord(readChar(f))
-    inc(i)
+  TCode = string
+  TData = array[0..kRamSize, byte]
 
 proc main(path:string) =
   var
+    code : TCode
+    data : TData
     ip  : int = 0 # instruction pointer
     dp  : int = 0 # data pointer
 
   proc fwd =
     var bal : int = 0; var done : bool
-    while ip < kRamSize and not done:
-      if chr(ram[ip]) == '[': inc(bal)
-      elif chr(ram[ip]) == ']':
+    while ip < len(code) and not done:
+      if code[ip] == '[': inc(bal)
+      elif code[ip] == ']':
         dec(bal)
         done = bal == 0
       inc(ip)
@@ -36,27 +28,27 @@ proc main(path:string) =
 
   proc bak =
     var bal : int = 0; var done : bool
-    while ip >= 0 and not done:
-      if chr(ram[ip]) == ']': inc(bal)
-      elif chr(ram[ip]) == '[':
+    while not done:
+      if code[ip] == ']': inc(bal)
+      elif code[ip] == '[':
         dec(bal)
         done = bal == 0
-      dec(ip)
-    if not done: raise newException(E_base, "No matching [ found.")
+      if ip == 0: raise newException(E_base, "No matching [ found.")
+      else: dec(ip)
 
-  readCode(path)
-  while ip < kRamSize:
-    case chr(ram[ip])
+  code = readFile(path)
+  while ip < len(code):
+    case code[ip]
     of '>' : inc dp
     of '<' : dec dp
-    of '+' : inc ram[dp]
-    of '-' : dec ram[dp]
-    of '.' : write stdout, chr(ram[dp])
-    of ',' : ram[dp] = ord(readChar(stdin))
+    of '+' : inc data[dp]
+    of '-' : dec data[dp]
+    of '.' : write stdout, chr(data[dp])
+    of ',' : data[dp] = ord(readChar(stdin))
     of '[' :
-      if ram[dp]!=0: fwd()
+      if data[dp]!=0: fwd()
     of ']' :
-      if ram[dp]==0: bak()
+      if data[dp]==0: bak()
     else   : nil
     inc(ip)
   echo()
